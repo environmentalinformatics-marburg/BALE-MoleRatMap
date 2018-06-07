81085b8b1eb8a0c57d11254c84f2bf90d56dfe1d
Thomas Nauss <nauss@staff.uni-marburg.de>
2018-06-07 15:53:12 +0200

    Add scaling

--- src/sentinel.R
+++ src/sentinel.R
@@ -2,12 +2,15 @@ library(mapview)
 library(raster)
 library(rgdal)
 
-path = "D:/tnauss/L1C_T37NEH_A012816_20171205T075207/S2A_MSIL1C_20171205T075211_N0206_R092_T37NEH_20171205T162810.SAFE/GRANULE/L1C_T37NEH_A012816_20171205T075207/IMG_DATA"
+path = "D:/tnauss/L1C_T37NEH_A012816_20171205T075207/S2A_MSIL2A_20171205T075211_N0206_R092_T37NEH_20171205T162810.SAFE/GRANULE/L2A_T37NEH_A012816_20171205T075207/IMG_DATA/R10m"
 files = list.files(path, full.names = TRUE, pattern = glob2rx("*.jp2"))
+sent = stack(files)
 
-sent2 = raster(files[2])
+# sent2 = raster(files[2])
+# mapview(sent[[1]])
 
-sent = stack(files[2:4])
+# Center and scale data
+sent_scale = scale(sent, center = TRUE, scale = TRUE)
+sent_scale
 
-
-mapview(sent[[1]])
+writeRaster(sent_scale, filename = "D:/tnauss/sentinel/", format = "GTiff", bylayer = TRUE)
